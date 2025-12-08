5d68075113343aca1b34e62da22a038a25ba9374
rhekdms <rhekdms0507@gmail.com>
2025-12-08 10:14:28 +0900

    test

---
+++ MiniGame/src/test/java/edu/mokpo/minigame/MiniGameApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.mokpo.minigame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
